4a0f5dbc608c149e8059184b906d10c199f1e42a
lambda-z <18048587325@163.com>
2026-02-21 20:24:25 +0800

    v1.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import {Button} from "my-react-lib";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,19 +8,8 @@ function App() {
   return (
     <>
       <div>
+          <Button label="Click me" onClick={() => setCount(count + 1)}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      include: ['src'],
+      outDir: 'dist',
+      entryRoot: 'src',
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
@@ -11,10 +19,8 @@ export default defineConfig({
       fileName: (format) => `index.${format === 'es' ? 'mjs' : 'js'}`,
     },
     rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
       external: ['react', 'react-dom', 'jsx-runtime'],
       output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供全局变量
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
